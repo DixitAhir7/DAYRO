90ff416e4b851b1d9f723ba7c578b2be706a1844
dixit-ahir <dixitahir0050@gmail.com>
2025-04-08 13:09:36 +0530

    all functions stored in seprate file

---
+++ DAYRO/functions-flie/functions.js
@@ -0,0 +1,119 @@
+// all validation here
+
+// login-form validation
+
+export function validatelogin() {
+    const submitBtn = document.querySelector(".submit");
+    const emailInput = document.querySelector(".email");
+    const passwordInput = document.querySelector(".password");
+    const modal = document.getElementById("loginModal");
+
+    try {
+        submitBtn.addEventListener("click", function () {
+
+            let obj = {
+                emailvalue: emailInput.value,
+                passwordvalue: passwordInput.value
+            }
+
+            if (!obj.emailvalue || !obj.passwordvalue) {
+                return alert("Please enter both email and password.");
+            }
+
+            // saving data and sending it to login-page
+
+            if (obj.emailvalue && obj.passwordvalue) {
+                localStorage.setItem('loginInfo', JSON.stringify(obj));
+                window.location.href = "login.html";
+                console.log('login succesful');
+                return false;
+            }
+
+            if (obj.passwordvalue.length < 5) {
+                return alert("Password must be at least 5 characters long.");
+            }
+
+            modal.style.display = "none";
+        });
+    } catch (e) {
+        console.log('got login error', e);
+    }
+}
+
+
+export function dropdownBtn() {
+    try {
+        window.toggleDropdown = function () {
+            let submenu = document.getElementById("submenu");
+            submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+        };
+    } catch (e) {
+        console.warn('dropdown did not worked', e);
+    };
+};
+
+
+// this script is for sidebar
+
+export function sidebarBtn() {
+
+    try {
+        const sidebar = document.querySelector('.sidebar');
+        const collapseBtn = document.querySelector('.sidebarToggle');
+
+        document.addEventListener('DOMContentLoaded', () => {
+            collapseBtn.addEventListener('click', (e) => {
+
+                e.preventDefault();
+                sidebar.classList.toggle('sidebar-collapse');
+            });
+        });
+    } catch (e) {
+        console.info("sidebar didn't opened", e);
+    }
+};
+
+
+// when user enter first time!
+
+export function firstTime() {
+    document.addEventListener("DOMContentLoaded", function () {
+
+        if (!localStorage.getItem("visited")) {
+            console.log("User visiting for the first time!");
+            alert("Welcome to our website for the first time!");
+
+            localStorage.setItem("visited", "true");
+
+        } else {
+            console.log("Welcome back!");
+        }
+    });
+}
+
+
+// this is modal script
+export function popUp() {
+
+    try {
+        const modal = document.getElementById("loginModal");
+        const openModalBtn = document.getElementById("openmodal");
+        const closeModalBtn = document.querySelector(".close");
+
+        openModalBtn.addEventListener("click", function () {
+            modal.style.display = "block";
+        });
+
+        closeModalBtn.addEventListener("click", function () {
+            modal.style.display = "none";
+        });
+
+        window.addEventListener("click", function (event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+    } catch (e) {
+        console.warn(e);
+    };
+};
--- DAYRO/js/script.js
+++ DAYRO/js/script.js
@@ -1,76 +1,14 @@
 import { userTime } from "../time/time-script/time.js";
-import { validatelogin } from "../validation-file/validate.js";
+import { validatelogin } from "../functions-flie/functions.js";
+import { dropdownBtn } from "../functions-flie/functions.js";
+import { sidebarBtn } from "../functions-flie/functions.js";
+import { firstTime } from "../functions-flie/functions.js";
+import { popUp } from "../functions-flie/functions.js";
 
 
-// this is modal script
-
-const modal = document.getElementById("loginModal");
-const openModalBtn = document.getElementById("openmodal");
-const closeModalBtn = document.querySelector(".close");
-
-
-openModalBtn.addEventListener("click", function () {
-    modal.style.display = "block";
-});
-
-closeModalBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-});
-
-window.addEventListener("click", function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-});
-
-
-// for login-validation
-
-validatelogin()
-
-// this is script for dropdown
-
-window.toggleDropdown = function () {
-    let submenu = document.getElementById("submenu");
-    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-};
-
-
-// this script is for sidebar
-
-const sidebar = document.querySelector('.sidebar');
-const collapseBtn = document.querySelector('.sidebarToggle');
-
-document.addEventListener('DOMContentLoaded', () => {
-    collapseBtn.addEventListener('click', (e) => {
-
-        e.preventDefault();
-        sidebar.classList.toggle('sidebar-collapse');
-
-        // if (sidebar.classList.contains('sidebar-collapse')) {
-        //     sidebar.innerHTML = `<a class="text-black sidebarToggle" href=""><i class="bi bi-list sidebarBtn"></i></a>`
-        // }else{
-        //     sidebar.innerHTML = `<i class="bi bi-arrow-right-circle-fill"></i>`
-        // }
-    });
-});
-
-
-// when user enter first time!
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    if (!localStorage.getItem("visited")) {
-        console.log("User visiting for the first time!");
-        alert("Welcome to our website for the first time!");
-
-        localStorage.setItem("visited", "true");
-
-    } else {
-        console.log("Welcome back!");
-    }
-});
-
-
-// count spending-time
-userTime() 
+popUp(); //modal script
+validatelogin() // for login-validation
+dropdownBtn(); // for dropdown
+sidebarBtn(); // sidebar-script
+firstTime(); //user visiting first-time
+userTime() // count spending-time
